f37ce80770337e08a1e8ee6a0ede57c0a2b65436
ewaoche <radicemmy@gmail.com>
2023-03-15 02:40:20 -0700

    commits

--- models/Participant.js
+++ models/Participant.js
@@ -2,92 +2,20 @@ const mongoose = require('mongoose');
 const moment = require('moment');
 
 const ParticipantSchema = new mongoose.Schema({
-  pid: {
+  name: {
     type: String
   },
   title: {
     type: String
   },
-nin:{type: Number},
-  firstName: {
-    type: String,
 
-  },
-  lastName: {
-    type: String
-
-  },
-  address: {
-    type: String,
-
-  },
-  country: {
-    type: String,
-
-  },
-  state: {
-    type: String,
-
-  },
-  lga: {
-    type: String,
-
-  },
-  ward: {
-    type: String,
-
-  },
-  dob: {
-    type: String,
-    default: moment().format("dddd Do MMMM YYYY")
-
-  },
-  gender: {
-    type: String,
-    required: [true, 'Please add an gender'],
-
-  },
-  gsm: {
-    type: String,
-    required: [true, 'Please add an gsm'],
-
-  },
-  natureOfBiz: {
-    type: String,
-    required: [true, 'Please add an natureOfBiz'],
-
-  },
-  occupation: {
-    type: String,
-    required: [true, 'Please add an occupation'],
-
-  },
-  email: {
-    type: String,
-    required: [true, 'Please add an email'],
-    unique: true,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      'Please add a valid email'
-    ]
-  },
- 
   createdAt: {
     type: String,
    default: moment().format("dddd Do MMMM YYYY")
   }
 }, 
 
-{
-  toJSON: {
-      transform(doc, ret){
-          ret.id = ret._id;
-          delete ret.password;
-          delete ret.__v;
-          delete  ret._id;
-      }
-  }
-}
+
 );
 
 
