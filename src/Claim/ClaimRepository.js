c19c0e75ecf654be07a8bca4e03dfbc3a9da56d6
MAHADI ABUHURAIRA <mamt4real@gmail.com>
2023-03-16 16:42:25 +0100

    Using Repository pattern

---
+++ src/Claim/ClaimRepository.js
@@ -0,0 +1,29 @@
+const { MongoDBCrudRepository } = require('../General/GeneralRepository')
+const Claim = require('../../models/Claim')
+
+const storage_type = process.env.DB_TYPE || 'mongodb'
+
+class ClaimMongoRepository {
+  #mongoCrudRepo = new MongoDBCrudRepository(Claim)
+  getOneClaim = async (id) =>
+    this.#mongoCrudRepo.getOne(id, [['participant', 'name title'], ['policy']])
+  getAllClaims = this.#mongoCrudRepo.getAll
+  updateClaim = async (id, data) =>
+    this.#mongoCrudRepo.updateOne(id, data, ['status', 'participant'])
+  createClaim = this.#mongoCrudRepo.createOne
+  deleteClaim = this.#mongoCrudRepo.deleteOne
+}
+
+class ClaimRepository {
+  static getRepositoryInstance() {
+    switch (storage_type) {
+      case 'mongodb':
+        return new ClaimMongoRepository()
+
+      default:
+        throw new Error('DB_TYPE is not defined')
+    }
+  }
+}
+
+module.exports = ClaimRepository
---
+++ src/General/GeneralRepository.js
@@ -0,0 +1,86 @@
+import { Model, Document } from 'mongoose'
+import { NotFound } from '../errors'
+import QueryHandler from './queryHandler'
+
+/**
+ * Handles CRUD operations for MongoDB
+ */
+export class MongoDBCrudRepository {
+  /**
+   *
+   * @param {Model} Model
+   */
+  constructor(Model) {
+    this.#Model = Model
+  }
+
+  /**
+   * Retrieve all documents
+   * @param {object} query a key value object used in filtering, sorting, projection, pagination
+   *
+   * @returns array of documents
+   */
+  async getAll(query = {}) {
+    const Processor = new QueryHandler(this.#Model, query)
+    let results = await Processor.process()
+    return results
+  }
+
+  /**
+   * Retrive a single document bas on Id
+   * @param {string | import('mongoose').ObjectId} id id of the document to fetch
+   * @param {string[][]} populateOptions list of populate arguments
+   */
+  async getOne(id, populateOptions = []) {
+    let query = this.#Model.findById(id)
+    if (populateOptions) {
+      populateOptions.forEach((option) => {
+        query = query.populate(...option)
+      })
+    }
+    const doc = await query
+    if (!doc)
+      throw new NotFound(`No ${this.#Model.modelName} with id: ${id} found!`)
+    return doc
+  }
+
+  /**
+   * Creates a new Document
+   * @param {object} data object containing required fields
+   * @returns {Document} new Document created
+   */
+  async createOne(data) {
+    const newDoc = await this.#Model.create(data)
+    return newDoc
+  }
+
+  /**
+   *
+   * @param {string | import('mongoose').ObjectId} id id of the document to update
+   * @param {object} data object containing required fields to update
+   * @param {string[]} forbiddenFields array of fields not allowed to be edited
+   * @returns {Document} updated Document
+   */
+  async updateOne(id, data, forbiddenFields = []) {
+    forbiddenFields.forEach((field) => {
+      if (req.body[field]) delete req.body[field]
+    })
+
+    const doc = await this.#Model.findByIdAndUpdate(id, data, {
+      new: true,
+      runValidators: true,
+    })
+
+    if (!doc)
+      throw new NotFound(`No ${this.#Model.modelName} with id: ${id} found!`)
+    return doc
+  }
+
+  /**
+   * Deletes a document
+   * @param {string | import('mongoose').ObjectId} id id of the document to delete
+   */
+  async deleteOne(id) {
+    await this.#Model.findByIdAndDelete(id)
+  }
+}
